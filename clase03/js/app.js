30ff381a402e3c4bdab7c7b0fd2ebbe1bdc69847
Ozba Micniuk <gt.ozba@gmail.com>
2020-03-22 21:49:19 -0400

    clase 03

---
+++ clase03/js/app.js
@@ -0,0 +1,63 @@
+//alert("Atención");
+// console.log("Atención");
+// var nro01;
+// nro01=5;
+// console.log(nro01);
+// var nro02="hola ";
+// console.log(nro01);
+// nro01=5;
+// var resultado;
+// resultado=nro01+5;
+// console.log(resultado);
+// resultado=nro01-5;
+// console.log(resultado);
+// resultado=nro01*5;
+// console.log(resultado);
+// resultado=nro01/5;
+// console.log(resultado);
+// resultado=nro01+" - "+nro02;
+// console.log(resultado);
+// resultado=nro01*nro02;
+// console.log(resultado);
+// var numerico=5;
+// var cadena='hola';
+// var logico=false;
+
+// if (numerico>3)
+//   {
+//     console.log("es verdadero");
+//   } else {
+//     console.log("es falso");
+//   }
+// if (numerico>3 && cadena=="hola")
+//   {
+//     console.log("es verdadero");
+//   } else {
+//     console.log("es falso");
+//   }
+// if (numerico>2 || logico==true)
+//   {
+//     console.log("es verdadero");
+//   } else {
+//     console.log("es falso");
+//   }
+  // if (numerico)
+  //   {
+  //     console.log("es verdadero");
+  //   } else {
+  //     console.log("es falso");
+  //   }
+
+  // for (var i=0; i<6; i++)
+  //   {
+  //   console.log(i);
+  //   }
+var i=10;
+// while (i<5) {
+//   console.log(i);
+//   i++;
+// }
+do {
+  console.log(i);
+  i++;  
+} while (i<5) ;
